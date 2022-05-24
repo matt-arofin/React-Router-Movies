2920651cfac8560085c9279402ef0cd452ea8921
Matthew Arofin <Matt.arofin@gmail.com>
2022-05-24 10:07:24 -0400

    First commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
+import MovieCard from './Movies/MovieCard';
+import Movie from './Movies/Movie'
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -12,6 +14,7 @@ export default function App () {
       axios
         .get('http://localhost:5001/api/movies') // Study this endpoint with Postman
         .then(response => {
+          setMovieList(response.data)
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
@@ -31,6 +34,7 @@ export default function App () {
       <SavedList list={[ /* This is stretch */]} />
 
       <div>Replace this Div with your Routes</div>
+      <Movie movieList={movieList} />
     </div>
   );
 }
